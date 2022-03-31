1586dc905c1c4bf4bb51bee1fd3f973b1a828db7
AnggaDanarP <87705975+AnggaDanarP@users.noreply.github.com>
2022-03-31 08:55:06 +0700

    update

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/iXIA2WJkPg5UW-3uHBih3sDpdJ0KaY3U",
-      accounts: [ '294807eb263610c87ab3f69231986e640aecb24d81f50d82963da0abdb638dc8' ]
+      url: "your api from alchemy",
+      accounts: [ 'your private account' ]
     }
   }
 };
